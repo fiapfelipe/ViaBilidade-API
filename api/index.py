8aee98b91da0659cd50aa1169cd356bea20a5d4b
fiapfelipe <rm559175@fiap.com.br>
2025-05-04 23:09:31 -0300

    atualização api

--- api/index.py
+++ api/index.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from api.login import login
 from api.manutencoes import manutencoes
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
@@ -13,8 +15,11 @@ def listar_usuarios():
     usuarios = [{'id': u['id'], 'username': u['username']} for u in login]
     return jsonify(usuarios), 200
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST', 'OPTIONS'])
 def fazer_login():
+    if request.method == 'OPTIONS':
+        return '', 200
+
     dados = request.get_json()
     username = dados.get('username')
     senha = dados.get('senha')
@@ -29,16 +34,22 @@ def fazer_login():
 def listar_manutencoes():
     return jsonify(manutencoes), 200
 
-@app.route('/manutencoes', methods=['POST'])
+@app.route('/manutencoes', methods=['POST', 'OPTIONS'])
 def adicionar_manutencao():
+    if request.method == 'OPTIONS':
+        return '', 200
+
     nova = request.get_json()
     nova['id'] = max(m['id'] for m in manutencoes) + 1 if manutencoes else 1
     nova['status'] = 'pendente'
     manutencoes.append(nova)
     return jsonify({'msg': 'Manutenção adicionada com sucesso'}), 201
 
-@app.route('/manutencoes/<int:id>', methods=['PUT'])
+@app.route('/manutencoes/<int:id>', methods=['PUT', 'OPTIONS'])
 def atualizar_status(id):
+    if request.method == 'OPTIONS':
+        return '', 200
+
     dados = request.get_json()
     novo_status = dados.get('status')
 
@@ -54,8 +65,11 @@ def atualizar_status(id):
 
     return jsonify({'msg': 'Manutenção não encontrada'}), 404
 
-@app.route('/manutencoes/<int:id>', methods=['DELETE'])
+@app.route('/manutencoes/<int:id>', methods=['DELETE', 'OPTIONS'])
 def deletar_manutencao(id):
+    if request.method == 'OPTIONS':
+        return '', 200
+
     for m in manutencoes:
         if m['id'] == id:
             manutencoes.remove(m)
